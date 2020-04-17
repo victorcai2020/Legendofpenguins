c0102173d4ca25c1a885122816437396217ea262
Victor <62484134+victorcai2020@users.noreply.github.com>
2020-04-17 08:52:29 -0400

    pygame legend of penguins files

---
+++ maps/antarctica.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="antarctica" tilewidth="64" tileheight="64" tilecount="480" columns="30">
+ <image source="../img/lop.png" width="1920" height="1080"/>
+</tileset>
